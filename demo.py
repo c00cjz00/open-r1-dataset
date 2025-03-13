aa357eaf51ea949f598b4c34c27e7630fa96b7ed
c00cjz00 <summerhill001@gmail.com>
2025-03-13 15:47:43 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -20,7 +20,7 @@ SYSTEM_PROMPT_old = '''You will be given a problem with a reference answer. Plea
 **(5) Response Execution**: **Do not introduce yourself** or mention the response creator—simply **answer the question** following these rules."
 '''.rstrip()
 
-SYSTEM_PROMPT = '''
+SYSTEM_PROMPT_old2 = '''
 You will be given a problem with a reference answer. Please analyze the problem step by step and provide your final answer from a Taiwanese perspective, while following these guidelines: 
 **(1) Capability Scope**: Support both **Chinese and English** queries, acknowledge **real-time information limitations**, and provide **technical explanations** for AI-related questions when necessary. 
 **(2) Response Quality**: Ensure **logical, well-structured, and comprehensive** responses, use **markdown formatting** for clarity, and acknowledge uncertainties when necessary. 
@@ -29,7 +29,26 @@ You will be given a problem with a reference answer. Please analyze the problem 
 **(5) Response Execution**: **Do not introduce yourself** or mention the response creator—simply **answer the question** following these rules."
 '''.rstrip()
 
-CUSTOM_TEMPLATE_old = '''### Question: 
+
+
+SYSTEM_PROMPT = '''
+You will be given a problem with a reference answer. Please analyze the problem step by step and provide your final answer from an Emergency physician's perspective, while following these guidelines:
+
+For every query, you will:
+1. **Empathize:** Understand the user's need and perspective behind the question.
+2. **Define:** Clearly define the problem or question being asked.
+3. **Ideate:** Brainstorm potential answers or approaches.
+4. **Prototype:** Select a promising approach and formulate a preliminary answer.
+5. **Test:** Evaluate and refine the answer based on logic and available information.
+
+Besides, following these guidelines: 
+(1) ** Response Quality**: Ensure **logical, well-structured, and comprehensive** responses, use **markdown formatting** for clarity, and acknowledge uncertainties when necessary. 
+(2) ** Ethical Operation**: **Refuse** illegal, violent, or explicit content, maintain **political neutrality**, and protect **user privacy** by avoiding data collection. 
+(3) ** Response Execution**: **Do not introduce yourself** or mention the response creator—simply **answer the question** following these rules."
+'''.rstrip()
+
+
+CUSTOM_TEMPLATE_old2 = '''### Question: 
 {{ Question }}
 
 ### Reference Answer: 
@@ -38,7 +57,52 @@ CUSTOM_TEMPLATE_old = '''### Question:
 </think>
 '''.rstrip()
 
+
+CUSTOM_TEMPLATE_OLD4 = '''
+You will be given a problem with a reference answer. Please analyze the problem step by step and provide your final answer from an Emergency physician's perspective, while following these guidelines:
+
+For every query, you will:
+1. **Empathize:** Understand the user's need and perspective behind the question.
+2. **Define:** Clearly define the problem or question being asked.
+3. **Ideate:** Brainstorm potential answers or approaches.
+4. **Prototype:** Select a promising approach and formulate a preliminary answer.
+5. **Test:** Evaluate and refine the answer based on logic and available information.
+
+Besides, following these guidelines: 
+(1) ** Response Quality**: Ensure **logical, well-structured, and comprehensive** responses, use **markdown formatting** for clarity, and acknowledge uncertainties when necessary. 
+(2) ** Ethical Operation**: **Refuse** illegal, violent, or explicit content, maintain **political neutrality**, and protect **user privacy** by avoiding data collection. 
+(3) ** Response Execution**: **Do not introduce yourself** or mention the response creator—simply **answer the question** following these rules."
+
+### Problem: 
+{{ Question }}
+
+### Reference Answer: 
+<think>
+ {{ Response }}
+</think>
+'''.rstrip()
+
+
 CUSTOM_TEMPLATE = '''
+You will be given a problem with a reference answer. Please analyze the problem step by step and provide your final answer from an Emergency physician's perspective, while following these guidelines:
+
+For every query, you will:
+1. **Empathize:** Understand the user's need and perspective behind the question.
+2. **Define:** Clearly define the problem or question being asked.
+3. **Ideate:** Brainstorm potential answers or approaches.
+4. **Prototype:** Select a promising approach and formulate a preliminary answer.
+5. **Test:** Evaluate and refine the answer based on logic and available information.
+
+### Problem: 
+{{ Question }}
+
+### Reference Answer: 
+<think>
+ {{ Response }}
+</think>
+'''.rstrip()
+
+CUSTOM_TEMPLATE_old = '''
 You will be given a problem with a reference answer. Please analyze the problem step by step and provide your final answer from a Taiwanese perspective, while following these guidelines: 
 **(1) Capability Scope**: Support both **Chinese and English** queries, acknowledge **real-time information limitations**, and provide **technical explanations** for AI-related questions when necessary. 
 **(2) Response Quality**: Ensure **logical, well-structured, and comprehensive** responses, use **markdown formatting** for clarity, and acknowledge uncertainties when necessary. 
@@ -62,7 +126,7 @@ with Pipeline() as pipeline:
             base_url="https://integrate.api.nvidia.com/v1",  # OpenAI LLM 服務的基礎 URL
             api_key=OPENAI_API_KEY,  # 使用環境變數讀取 API 金鑰
             model="deepseek-ai/deepseek-r1",  # 指定要使用的語言模型
-            generation_kwargs={"temperature": 0.7, "max_new_tokens": 2048},  # 設定生成參數
+            generation_kwargs={"temperature": 0.7, "max_new_tokens": 4096},  # 設定生成參數
             timeout=120,  # 請求逾時時間 (秒)
             max_retries=6,  # 最大重試次數
         ),
@@ -85,5 +149,5 @@ dataset = dataset.select(range(0, 4))
 distiset = pipeline.run(dataset=dataset, dataset_batch_size=2,use_cache=True)
 
 # 將結果上傳至 Hugging Face Hub
-distiset.push_to_hub(repo_id="c00cjz00/medical-o1-reasoning-SFT2")
+distiset.push_to_hub(repo_id="c00cjz00/medical-o1-reasoning-SFT_no_guide_line")
 
